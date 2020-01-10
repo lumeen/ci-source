1c0282c5c8d981d32a2e33f10189acc73c8f52b7
lumeen <mateusz.niescior@gmail.com>
2020-01-10 14:06:24 +0100

    Update python.py

--- scripts/python.py
+++ scripts/python.py
@@ -4,7 +4,7 @@ import os
 print (os.environ['muleUsername'])
 print (os.environ['mulePassword'])
 url = 'https://anypoint.mulesoft.com/accounts/login'
-myobj = {'username': os.environ['muleUsername'], password:os.environ['mulePassword']}
+myobj = {'username': os.environ['muleUsername'], 'password':os.environ['mulePassword']}
 x = requests.post(url, data = myobj)
 
 print(x.json())
