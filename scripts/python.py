3d471039af64fba379d2dd6d5a6ba1a461d880d0
lumeen <mateusz.niescior@gmail.com>
2020-01-10 14:05:05 +0100

    Update python.py

--- scripts/python.py
+++ scripts/python.py
@@ -3,18 +3,9 @@ import os
 
 print (os.environ['muleUsername'])
 print (os.environ['mulePassword'])
-url = 'https://www.w3schools.com/python/demopage.php'
-myobj = {'somekey': 'somevalue'}
-print("Hello World!")
+url = 'https://anypoint.mulesoft.com/accounts/login'
+myobj = {'username': os.environ['muleUsername'], password:os.environ['mulePassword']}
 x = requests.post(url, data = myobj)
-r = requests.get('https://jsonplaceholder.typicode.com/todos/1')
-print (1)
-r.text
-print (2)
 
-print(x.text)
-print (3)
-
-print(r.json())
-print (4)
+print(x.json())
 
