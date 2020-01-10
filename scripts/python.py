e362afb7d16a6fb80f387976817961b05e784a7f
lumeen <mateusz.niescior@gmail.com>
2020-01-10 13:17:00 +0100

    Update python.py

--- scripts/python.py
+++ scripts/python.py
@@ -2,7 +2,7 @@ import requests
 
 url = 'https://www.w3schools.com/python/demopage.php'
 myobj = {'somekey': 'somevalue'}
-
+print("Hello World!")
 x = requests.post(url, data = myobj)
 
 print(x.text)
