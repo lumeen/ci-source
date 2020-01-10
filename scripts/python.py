7121777280bf202438e1f01623c2ae2dcbdc679c
lumeen <mateusz.niescior@gmail.com>
2020-01-10 13:20:48 +0100

    Update python.py

--- scripts/python.py
+++ scripts/python.py
@@ -4,5 +4,7 @@ url = 'https://www.w3schools.com/python/demopage.php'
 myobj = {'somekey': 'somevalue'}
 print("Hello World!")
 x = requests.post(url, data = myobj)
-
+r = requests.get('https://github.com/timeline.json')
+r.text
 print(x.text)
+print(r)
