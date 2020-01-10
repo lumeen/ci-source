56c92d02a5e091fbb6e8c2819098974ba5be0490
lumeen <mateusz.niescior@gmail.com>
2020-01-10 14:22:00 +0100

    Update python.py

--- scripts/python.py
+++ scripts/python.py
@@ -5,20 +5,20 @@ url = 'https://anypoint.mulesoft.com/accounts/login'
 myobj = {'username': os.environ['muleUsername'], 'password':os.environ['mulePassword']}
 x = requests.post(url, data = myobj)
 
-token = x.json()['access_token']
+token = 'Bearer ' + x.json()['access_token']
 print(token)
 
 
 url = 'https://anypoint.mulesoft.com/accounts/login'
 
-headers = {'Authorization': 'application/json'}
+headers = {'Authorization': token, 'X-ANYPNT-ENV-ID': '2b38afe9-1e88-411e-82d7-b9376cfab625'}
 
 files = {'file': ('report.csv', 'data to send')}
 
 response = requests.post(url, data={
    'autoStart' :'true',
    'appInfoJson':'{    "domain": "helloo",    "muleVersion": {        "version": "4.2.2"    },    "region": "us-east-1",    "monitoringEnabled": true,    "monitoringAutoRestart": true,    "workers": {        "amount": 1,        "type": {            "name": "Medium",            "weight": 0.2,            "cpu": "0.2 vCore",            "memory": "500 MB memory"        }    },    "loggingNgEnabled": true,    "persistentQueues": false,    "objectStoreV1": false}'
-   }, files=files)
+   }, files=files, headers=headers)
 
 
 print(response.json())
