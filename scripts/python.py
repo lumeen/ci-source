681a8502964fc9fe98f03450a34a7f382921c5fc
lumeen <mateusz.niescior@gmail.com>
2020-01-10 13:27:53 +0100

    Update python.py

--- scripts/python.py
+++ scripts/python.py
@@ -4,7 +4,7 @@ url = 'https://www.w3schools.com/python/demopage.php'
 myobj = {'somekey': 'somevalue'}
 print("Hello World!")
 x = requests.post(url, data = myobj)
-r = requests.get('https://github.com/timeline.json')
+r = requests.get('https://jsonplaceholder.typicode.com/todos/1')
 print (1)
 r.text
 print (2)
