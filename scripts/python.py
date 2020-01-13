8d541cbcb755d66756717c1eae5bf4e9d75c8752
lumeen <mateusz.niescior@gmail.com>
2020-01-13 13:25:41 +0100

    Update python.py

--- scripts/python.py
+++ scripts/python.py
@@ -2,28 +2,26 @@ import requests
 import os
 import json
 
-url = 'https://anypoint.mulesoft.com/accounts/login'
-myobj = {'username': os.environ['muleUsername'], 'password':os.environ['mulePassword']}
-x = requests.post(url, data = myobj)
+loginUrl = 'https://anypoint.mulesoft.com/accounts/login'
+deploy = 'https://anypoint.mulesoft.com/hybrid/api/v1//applications'
+
+loginObject = {'username': os.environ['muleUsername'], 'password':os.environ['mulePassword']}
+x = requests.post(loginUrl, data = loginObject)
 
 token = 'Bearer ' + x.json()['access_token']
-print(token)
-
 
 url = 'https://anypoint.mulesoft.com/hybrid/api/v1//applications'
-for entry in os.scandir('.'):
-        print(entry.name)
+
 headers = {'Authorization': token, 'X-ANYPNT-ENV-ID': '2b38afe9-1e88-411e-82d7-b9376cfab625', 'X-ANYPNT-ORG-ID':'88063e25-29df-47cc-b930-c4c75ee17938'}
 files = {'file': open('maven-output/hello-1.0.0-SNAPSHOT-mule-application.jar','rb')}
-response = requests.post(url, data={
+response = requests.post(deploy, data={
    'targetId' :'2298399',
    'artifactName' :'aaaaaaa'},   
   files=files, headers=headers)
 
+if(response.status_code != '202') {
+raise Exception('x should not exceed 5. The value of x)
+}
 
-print(response.json())
-print(response)
-print(response.status_code)
-print(response.reason)
-print(response.encoding)
+
 
