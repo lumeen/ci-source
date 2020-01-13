5a2dfcd5ec27194ee6e90bb9fab41fc5f75ac487
lumeen <mateusz.niescior@gmail.com>
2020-01-13 15:05:08 +0100

    Update python.py

--- scripts/python.py
+++ scripts/python.py
@@ -3,16 +3,34 @@ import os
 import json
 
 loginUrl = 'https://anypoint.mulesoft.com/accounts/login'
-deploy = 'https://anypoint.mulesoft.com/hybrid/api/v1//applications'
+applicationUrl = 'https://anypoint.mulesoft.com/hybrid/api/v1//applications'
+
 
 loginObject = {'username': os.environ['muleUsername'], 'password':os.environ['mulePassword']}
-x = requests.post(loginUrl, data = loginObject)
-
-token = 'Bearer ' + x.json()['access_token']
-
+loginResponse = requests.post(loginUrl, data = loginObject)
+token = 'Bearer ' + loginResponse.json()['access_token']
 headers = {'Authorization': token, 'X-ANYPNT-ENV-ID': '2b38afe9-1e88-411e-82d7-b9376cfab625', 'X-ANYPNT-ORG-ID':'88063e25-29df-47cc-b930-c4c75ee17938'}
+
+applicationResposne = requests.get(applicationUrl, headers = headers)
+
+print(applicationResposne.json())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 files = {'file': open('maven-output/hello-1.0.0-SNAPSHOT-mule-application.jar','rb')}
-response = requests.post(deploy, data={
+response = requests.post(applicationUrl, data={
    'targetId' :'2298399',
    'artifactName' :'aaaaaaa'},   
   files=files, headers=headers)
@@ -21,7 +39,3 @@ responseStatus = response.json()['message']
 print(responseStatus) 
 if response.status_code != 202:
    raise Exception('Error during deploment: ' + responseStatus)
-
-
-
-
