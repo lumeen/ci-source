f8377a985ea4862336acc1d81f0abfe45e1d4950
lumeen <mateusz.niescior@gmail.com>
2020-01-13 12:21:06 +0100

    Update python.py

--- scripts/python.py
+++ scripts/python.py
@@ -15,7 +15,7 @@ url = 'https://anypoint.mulesoft.com/hybrid/api/v1//applications'
 headers = {'Authorization': token, 'X-ANYPNT-ENV-ID': '2b38afe9-1e88-411e-82d7-b9376cfab625', 'X-ANYPNT-ORG-ID':'88063e25-29df-47cc-b930-c4c75ee17938'}
 files = {'file': ('maven-output/hello-1.0.0-SNAPSHOT-mule-application.jar', 'application jar')}
 response = requests.post(url, data={
-   'targetId' :'2298348',
+   'targetId' :'2298399',
    'artifactName' :'testowa'},   
   files=files, headers=headers)
 
