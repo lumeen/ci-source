8c579da0d1768d8eee3130876586f69e7b3e6f53
lumeen <mateusz.niescior@gmail.com>
2020-01-14 12:16:17 +0100

    Update python.py

--- scripts/python.py
+++ scripts/python.py
@@ -34,5 +34,5 @@ else:
    response = requests.patch(applicationUrl + "/" + str(applicationId),
       files=files, headers=headers)
    print(response.status_code)
-   if response.status_code != 202:
+   if response.status_code != 200:
       raise Exception('Error during deploment: ' + response.reason)
