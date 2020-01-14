0258cd04accd09d2bf3c7038a3f4a7e51b7236d3
lumeen <mateusz.niescior@gmail.com>
2020-01-14 09:18:01 +0100

    Update python.py

--- scripts/python.py
+++ scripts/python.py
@@ -18,10 +18,10 @@ applicationProperties = next((x for x in applications if x['name'] == 'aaaaaaa')
 applicationId =  applicationProperties['id'] if applicationProperties != None else None
 files = {'file': open('maven-output/hello-1.0.0-SNAPSHOT-mule-application.jar','rb')}
 
-   print("wchodze1")
+print("wchodze1")
 
 if applicationId == None: 
-      print("wchodze2")
+   print("wchodze2")
    response = requests.post(applicationUrl, data={
    'targetId' :'2298399',
    'artifactName' :'aaaaaaa'},   
